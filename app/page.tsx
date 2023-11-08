877a9c2346e190a92d41bf0457c67e654b9ba4cd
mingke-utas <mingked@utas.edu.au>
2023-11-08 12:15:43 +1100

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,204 +1,55 @@
-import { getProductById } from '../lib/actions'
-import { redirect } from 'next/navigation'
-import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Product } from '../types'
-import { formatNumber } from '../lib/utils'
-import PriceCard from '../components/PriceCard'
-import { getSimilars } from '../lib/actions'
-import ProductCard from '../components/ProductCard'
-import Modal from '../components/Modal'
+import React from "react"
+import HeroCarousel from "../components/HeroCarousel"
+import SearchBar from "../components/SearchBar"
+import Image from "next/image"
+import { getAll } from "../lib/actions"
+import ProductCard from "../components/ProductCard"
 
-type Props= {
-    params : {id : string}
-}
-const ProductDetails = async ({params:{id}}:Props) => {
- const product : Product = await getProductById(id)
- if(!product) redirect('/')
- const similaries = await getSimilars(id)
+const Home = async () => {
+  const allProducts = await getAll();
+
   return (
-    <div className="product-container">
-      <div className="flex gap-28 xl:flex-row flex-col">
-        <div className="product-image">
-          <Image 
-            src={product.image}
-            alt={product.title}
-            width={580}
-            height={400}
-            className="mx-auto"
-          />
-        </div>
+    <>
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
+          <div className="flex flex-col justify-center"> 
+            <p className="small-text">
+              Smart Shopping Starts Here:
+              <Image 
+                src="/assets/icons/arrow-right.svg"
+                alt="arrow-right"
+                width={16}
+                height={16}
+              />
+            </p>
 
-        <div className="flex-1 flex flex-col">
-          <div className="flex justify-between items-start gap-5 flex-wrap pb-6">
-            <div className="flex flex-col gap-3">
-              <p className="text-[28px] text-secondary font-semibold">
-                {product.title}
-              </p>
+            <h1 className="head-text">
+              Unleash the Power of
+              <span className="text-primary"> PriceWise</span>
+            </h1>
 
-              <Link
-                href={product.url}
-                target="_blank"
-                className="text-base text-black opacity-50"
-              >
-                Visit Product
-              </Link>
-            </div>
+            <p className="mt-6">
+              Powerful, self-serve product and growth analytics to help you convert, engage, and retain more.
+            </p>
 
-            <div className="flex items-center gap-3">
-              <div className="product-hearts">
-                <Image 
-                  src="/assets/icons/red-heart.svg"
-                  alt="heart"
-                  width={20}
-                  height={20}
-                />
-
-                <p className="text-base font-semibold text-[#D46F77]">
-                  {product.reviewsCount}
-                </p>
-              </div>
-
-              <div className="p-2 bg-white-200 rounded-10">
-                <Image 
-                  src="/assets/icons/bookmark.svg"
-                  alt="bookmark"
-                  width={20}
-                  height={20}
-                />
-              </div>
-
-              <div className="p-2 bg-white-200 rounded-10">
-                <Image 
-                  src="/assets/icons/share.svg"
-                  alt="share"
-                  width={20}
-                  height={20}
-                />
-              </div>
-            </div>
+            <SearchBar />
           </div>
 
-          <div className="product-info">
-            <div className="flex flex-col gap-2">
-              <p className="text-[34px] text-secondary font-bold">
-                {product.currency} {formatNumber(product.currentPrice)}
-              </p>
-              <p className="text-[21px] text-black opacity-50 line-through">
-                {product.currency} {formatNumber(product.originPrice)}
-              </p>
-            </div>
-
-            <div className="flex flex-col gap-4">
-              <div className="flex gap-3">
-                <div className="product-stars">
-                  <Image 
-                    src="/assets/icons/star.svg"
-                    alt="star"
-                    width={16}
-                    height={16}
-                  />
-                  <p className="text-sm text-primary-orange font-semibold">
-                    {product.stars || '25'}
-                  </p>
-                </div>
-
-                <div className="product-reviews">
-                  <Image 
-                    src="/assets/icons/comment.svg"
-                    alt="comment"
-                    width={16}
-                    height={16}
-                  />
-                  <p className="text-sm text-secondary font-semibold">
-                    {product.reviewsCount} Reviews
-                  </p>
-                </div>
-              </div>
-
-              <p className="text-sm text-black opacity-50">
-                <span className="text-primary-green font-semibold">93% </span> of
-                buyers have recommeded this.
-              </p>
-            </div>
-          </div>
-
-          <div className="my-7 flex flex-col gap-5">
-            <div className="flex gap-5 flex-wrap">
-              <PriceCard 
-                title="Current Price"
-                iconSrc="/assets/icons/price-tag.svg"
-                value={`${product.currency} ${formatNumber(product.currentPrice)}`}
-              />
-              <PriceCard 
-                title="Average Price"
-                iconSrc="/assets/icons/chart.svg"
-                value={`${product.currency} ${formatNumber(product.averagePrice)}`}
-              />
-              <PriceCard 
-                title="Highest Price"
-                iconSrc="/assets/icons/arrow-up.svg"
-                value={`${product.currency} ${formatNumber(product.highestPrice)}`}
-              />
-              <PriceCard 
-                title="Lowest Price"
-                iconSrc="/assets/icons/arrow-down.svg"
-                value={`${product.currency} ${formatNumber(product.lowestPrice)}`}
-              />
-            </div>
-          </div>
-          <Modal productId={id}/>
+          <HeroCarousel />
         </div>
-      </div>
-      <div className='flex flex-col gap-16 border-l-black'>
-        <div className='flex flex-col gap-5'>
-            <h3 className='text-2xl text-secondary font-semibold'>
-                Product Description
-            </h3>
-            <div className='flex flex-col gap-4'>
-                {
-                    product?.description?.split('\n')
-                }
-            </div>
+      </section>
+
+      <section className="trending-section">
+        <h2 className="section-text">Trending</h2>
+
+        <div className="flex flex-wrap gap-x-8 gap-y-16">
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
         </div>
-        <button className='btn w-fit mx-auto flex items-center justify-center gap-3 min-w-[200px]' type='button'>
-            <Image
-                src="/assets/icons/bag.svg"
-                alt="check"
-                width={22}
-                height={22}
-            />
-            <Link 
-                href='/'
-                className='text-base text-white'
-            >
-                Buy Now
-            </Link>
-        </button>
-      </div>
-        {
-            similaries && similaries?.length > 0 && (
-                <div className='py-14 flex flex-col gap-2 w-full'>
-                    <p className='section-text'>
-                        Similar Products
-                    </p>
-                    <div className='flex flex-wrap gap-10 mt-7 w-full'>
-                        {
-                            similaries.map((product)=>(
-                                <ProductCard
-                                    key={product._id}
-                                    product={product}
-                                />
-                            ))
-                        }
-                    </div>
-                </div>
-            )
-        }
-    </div>
+      </section>
+    </>
   )
-
 }
 
-export default ProductDetails
+export default Home
